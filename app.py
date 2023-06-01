76ec65df1f5c170da755a226048de5576ae69c8e
heferohe <134904707+heferohe@users.noreply.github.com>
2023-06-01 15:58:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -22,6 +22,20 @@ def home():
         # Aquí es donde procesarías el texto. Por ahora, solo devolvemos el mismo texto.
         source_language = ''
         translated_text = text
+        
+        params = {
+    'api-version': '3.0',
+    'from': 'en',
+    'to': ['fr', 'zu']
+                }
+
+headers = {
+    'Ocp-Apim-Subscription-Key': cog_key,
+    # location required if you're using a multi-service or regional (not global) resource.
+    'Ocp-Apim-Subscription-Region': cog_region,
+    'Content-type': 'application/json',
+    'X-ClientTraceId': str(uuid.uuid4())
+        }
 
         return render_template('home.html', translated_text=translated_text,lang_detected=source_language)
     
